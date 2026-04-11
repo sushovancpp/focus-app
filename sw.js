246b45cba3839acab5dd3ac1831cc1237df44e51
Sushovan <masantasushovan@gmail.com>
2026-04-11 21:43:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() =>  self.skipWaiting())
   );
-});
+}); 
  
 self.addEventListener('activate', event => {
   event.waitUntil(
